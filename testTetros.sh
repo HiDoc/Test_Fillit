7c6bea1a5865d8cb2ed315a1c879917520cad1de
Florian MADURA <fmadura@e1r9p3.42.fr>
2017-12-01 18:00:26 +0100

    add some files

--- testTetros.sh
+++ testTetros.sh
@@ -1,12 +1,25 @@
+#!/bin/bash
+
+path="../../Fillit/fillit"
+
+echo Tetrominos non valide
+echo *********************
 for file in ./TetroNotValid/*; 
 do
-	echo $file;
-   	/tmp/lchancri/fillit $file;	
-done;
-../../Fillit/fillit hello
-for file in ./TetroValid/*; 
-do
-	echo $file;
-   	/tmp/lchancri/fillit $file;	
+	$path $file | diff <(echo "error") -;	
 done;
 
+echo *********************
+echo "File not valid"
+$path hello
+
+echo *********************
+count=1
+echo Tetrominos valides
+echo *********************
+for file in ./TetroValid/*; 
+	do
+   		$path $file | diff FillitValid/"$count".txt -;
+		((count++));	
+done;
+echo *********************
